571843fa2097fab32352ea6fdfc55c9258df9064
Phenomenom05 <115370719+Phenomenom05@users.noreply.github.com>
2024-07-23 00:15:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -143,8 +143,8 @@ WebDriverWait(driver, 20).until(
 ).click()
 time.sleep(7)
 driver.get("chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html")
-time.sleep(8)
-WebDriverWait(driver, 20).until(
+time.sleep(10)
+WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div/div[3]/div[2]/footer/button[2]"))
 ).click()
 
@@ -159,20 +159,20 @@ driver.get("https://console.brahma.fi/")
 
 try:
     time.sleep(10)
-
-    WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[1]/div[2]/div/button"))
-    ).click()
-
-    driver.get("chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html")
-
-    WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[3]/div/div[5]/footer/button[2]"))
-    ).click()
-    
-    time.sleep(5)
-    driver.get("https://console.brahma.fi/")
-    time.sleep(10)
+    #
+    # WebDriverWait(driver, 20).until(
+    #     EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[1]/div[2]/div/button"))
+    # ).click()
+    #
+    # driver.get("chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html")
+    #
+    # WebDriverWait(driver, 20).until(
+    #     EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[3]/div/div[5]/footer/button[2]"))
+    # ).click()
+    #
+    # time.sleep(10)
+    # driver.get("https://console.brahma.fi/")
+    # time.sleep(500)
 
     ui_testing_console_button = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div/div[1]/div[2]/div/div[2]")
     WebDriverWait(driver, 30).until(
